1aa0e33af93e8ed21ca2c2b3ae2c0df8a73e4605
alecumitu <alecu.mitu@gmail.com>
2024-06-03 08:05:01 +0000

    Read survey excel sheet raw data

--- R/01-data_download.R
+++ R/01-data_download.R
@@ -1,3 +1,2 @@
-mtcars
-
-write.csv(mtcars, "data/carsdataset.csv")
+library(googlesheets4)
+read_sheet("https://docs.google.com/spreadsheets/d/1C4MI7v8Fdsk-2DESzuXcFfOcS1Bqu3c7A37WqOHomfk/edit?resourcekey#gid=408969245")
